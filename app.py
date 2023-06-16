ad1a927b0a188f7c6b56bb7994fe9c44483f23df
doxgxxn <praze74@gmail.com>
2023-06-16 10:21:05 +0900

    img

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
-
+# streamlit run app.py
 
 st.title("나의 파이썬 웹페이지")
 st.header("수업 8일차에 만들었어요")
 st.subheader("그래도 잘 만들었죠?")
 st.write("내가 만든 페이지, 너를 위해 구었지!!!")
-
-# streamlit run app.py
+st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")
+st.image("https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569_1280.jpg")
+st.video("https://i.imgur.com/nRZciOB.mp4")
+st.image("image/cat.jpeg",use_column_width=True)
+st.image("image/cat.jpeg", width=)
+st.image("image/cat.jpeg",channels="BGR")
